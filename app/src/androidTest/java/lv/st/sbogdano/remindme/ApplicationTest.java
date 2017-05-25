a5dc15235115c327f4d66aa15df1c0215abb47da
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2017-05-25 08:46:04 +0300

    Create MainActivity and layout

---
+++ app/src/androidTest/java/lv/st/sbogdano/remindme/ApplicationTest.java
@@ -0,0 +1,15 @@
+package lv.st.sbogdano.remindme;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by bogda on 25/05/2017.
+ */
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
