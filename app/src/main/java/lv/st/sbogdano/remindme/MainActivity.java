14081c24a10a73eec9b71361daef7560007baa99
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2017-05-25 14:45:35 +0300

    Added ToolBar and Search icon

--- app/src/main/java/lv/st/sbogdano/remindme/MainActivity.java
+++ app/src/main/java/lv/st/sbogdano/remindme/MainActivity.java
@@ -1,13 +1,34 @@
 package lv.st.sbogdano.remindme;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        mToolbar.inflateMenu(R.menu.menu);
     }
 }
