58d8f68232299ba4f4fba12d4d8da5817ff03be6
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2017-05-26 10:19:49 +0300

    Update Navigation Drawer added 1 item and header

--- app/src/main/java/lv/st/sbogdano/remindme/MainActivity.java
+++ app/src/main/java/lv/st/sbogdano/remindme/MainActivity.java
@@ -1,5 +1,6 @@
 package lv.st.sbogdano.remindme;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,15 +8,19 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolBar();
+        initNavigationView();
     }
 
     private void initToolBar() {
@@ -30,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mToolbar.inflateMenu(R.menu.menu);
+    }
+
+
+    private void initNavigationView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
